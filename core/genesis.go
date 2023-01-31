919af8c8ea2f2c67615d947d415055e1786cda03
topdefi <94203269+topdefi@users.noreply.github.com>
2023-01-31 14:10:18 -0800

    Update Genesis

--- core/genesis.go
+++ core/genesis.go
@@ -525,10 +525,10 @@ func (g *Genesis) MustCommit(db ethdb.Database) *types.Block {
 func DefaultGenesisBlock() *Genesis {
 	return &Genesis{
 		Config:     params.MainnetChainConfig,
-		Nonce:      66,
+		Nonce:      0,
 		ExtraData:  hexutil.MustDecode("0xb82184f2ad23a626c519921e579caebb6f9ea6e76165318372e639dc3ab797c7"),
 		GasLimit:   5000,
-		Difficulty: big.NewInt(17179869184),
+		Difficulty: big.NewInt(1),
 		Alloc:      decodePrealloc(mainnetAllocData),
 	}
 }
